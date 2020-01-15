e576b8a989bf506518055c50e7f57fd08b85dac4
tongwenjie <179355991@qq.com>
2020-01-15 09:46:18 +0800

    s

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Helpers\ResponseHelper;
+use App\Http\Service\TestService;
 use Mix\Http\Message\ServerRequest;
 use Mix\Http\Message\Response;
 
@@ -11,9 +12,9 @@ class TestController
     public function index(ServerRequest $request, Response $response)
     {
         $content = 'Hello, World!';
-        $db = context()->get(\Mix\Database\Connection::class);
-        $data = $db->prepare("SELECT * FROM `wj_users` where id = 1")->queryAll();
-        var_dump($data);
+
+        TestService::index();
+
         return ResponseHelper::html($response, $content);
     }
 }
--- app/Http/Service/TestService.php
+++ app/Http/Service/TestService.php
@@ -7,5 +7,10 @@ class TestService
     public static function index()
     {
 
+        for ($i=0;10;$i++) {
+            echo $i;
+            sleep(1);
+        }
+
     }
 }
