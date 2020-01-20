65fc6418a0833a2a0e1527c7db2cddc91395a7ae
tongwenjie <179355991@qq.com>
2020-01-20 10:03:07 +0800

    fox

--- views/test/test.php
+++ views/test/test.php
@@ -18,7 +18,7 @@ print_r($friends);
 
 <script>
     var webSocket = function () {
-        ws = new WebSocket("ws://39.108.6.204:9501");
+        ws = new WebSocket("ws://39.108.6.204:9502/websocket");
         ws.onopen = function() {
             console.log("连接成功");
         };
